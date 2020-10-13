fb77a300e19cb3fab046c5befb42d9b990e53898
Soontorn Choosenpom <soontorn.cho@mail.pbru.ac.th>
2020-10-13 21:26:54 +0700

    first commit

---
+++ lib/themes/style.dart
@@ -0,0 +1,11 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+      fontFamily: 'Montserrat',
+      primaryColor: Colors.blue,
+      accentColor: Colors.blueAccent,
+      buttonColor: Colors.lightBlue);
+}
